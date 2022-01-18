7784c6ddeef5904537e98e38c72ba5b0203d52f9
Carter Leising <carter.leising@perficient.com>
2022-01-18 17:12:06 -0600

    cleaned added get and set methods

--- techbootcamp-carterleising/src/main/java/com/perficient/techbootcampcarterleising/HockeyStats.java
+++ techbootcamp-carterleising/src/main/java/com/perficient/techbootcampcarterleising/HockeyStats.java
@@ -3,23 +3,61 @@ package com.perficient.techbootcampcarterleising;
 public class HockeyStats {
 	private static int goals;
 	private static int assists;
+	private static String name;
+	private static int shots;
+	private static int plusMinus;
 	
 
-	public static void main() {
-		
-		// TODO Auto-generated method stub
-
-	}
-	public static void setVal(int numGoals, int numAssists) {
-		goals = numGoals;
-		assists = numAssists;
+	public HockeyStats(int numGoals, int numAssists, String newName, int numShots, int valPlusMinus) {
+		setGoals(numGoals);
+		setAssists(numAssists);
+		setName(newName);
+		setShots(numShots);
+		setPlusMinus(valPlusMinus);
 	}
 	
+
 	public static int getGoals() {
 		return goals;
 	}
-	
+
+
+	public static void setGoals(int goals) {
+		HockeyStats.goals = goals;
+	}
+
+
 	public static int getAssists() {
 		return assists;
+	}
+
+
+	public static void setAssists(int assists) {
+		HockeyStats.assists = assists;
+	}
+
+
+	public static String getName() {
+		return name;
+	}
+
+	public static void setName(String name) {
+		HockeyStats.name = name;
+	}
+
+	public static int getShots() {
+		return shots;
+	}
+
+	public static void setShots(int shots) {
+		HockeyStats.shots = shots;
+	}
+
+	public static int getPlusMinus() {
+		return plusMinus;
+	}
+
+	public static void setPlusMinus(int plusMinus) {
+		HockeyStats.plusMinus = plusMinus;
 	}
 }
--- techbootcamp-carterleising/src/test/java/com/perficient/techbootcampcarterleising/HockeyStatsTests.java
+++ techbootcamp-carterleising/src/test/java/com/perficient/techbootcampcarterleising/HockeyStatsTests.java
@@ -1,29 +1,48 @@
 package com.perficient.techbootcampcarterleising;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class HockeyStatsTests {
-	HockeyStats stats;
+	private static HockeyStats stats;
+	private static final Logger logger = LogManager.getLogger(HockeyStats.class);
 	
 	@BeforeEach
 	void setUp() {
-		HockeyStats.setVal(4, 10);
+		stats = new HockeyStats(4, 10, "Carter", 37, 5);
+		
 	}
 	
 	@Test
 	@DisplayName("Goals pass")
 	void testGoalsPass() {
-		assertEquals(4, HockeyStats.getGoals(), "goals should equal");
+		logger.debug("logging in player goals {}", stats.getGoals());
+		assertEquals(4, stats.getGoals(), "goals should equal");
 	}
 	
 	@Test
 	@DisplayName("Goals fail")
 	void testGoalsFail() {
-		assertEquals(6, HockeyStats.getGoals(), "goals should equal");
+		assertNotEquals(6, stats.getGoals(), "goals should equal");
+	}
+	
+	@Test
+	@DisplayName("Assists pass")
+	void testAssistsPass() {
+		logger.debug("logging in player assists {}", stats.getAssists());
+		assertEquals(10, stats.getAssists(), "goals should equal");
+	}
+	
+	@Test
+	@DisplayName("Assists fail")
+	void testAssistsFail() {
+		assertNotEquals(6, stats.getAssists(), "goals should equal");
 	}
 }
